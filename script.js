56482745abdcc614928f866a69fbd9a36fbf7cf2
Bheemesh N S <bheeman902@gmail.com>
2025-11-27 15:28:32 +0530

    first repo

---
+++ script.js
@@ -0,0 +1,10 @@
+function upper(){
+    var input = document.getElementById("inp").value;
+    document.getElementById("out").innerText = input.toUpperCase();
+
+}
+
+function lower(){
+    var input = document.getElementById("inp").value;
+    document.getElementById("out").innerText = input.toLowerCase();
+}
