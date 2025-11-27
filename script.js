ff30a007b1ad2c26c10920e077ba21deec7d4bd1
Bheemesh N S <bheeman902@gmail.com>
2025-11-27 15:43:17 +0530

    added font size

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ function lower(){
     var input = document.getElementById("inp").value;
     document.getElementById("out").innerText = input.toLowerCase();
 }
+
+var size = 16;
+
+function incfont(){
+    var outt = document.getElementById("out");
+    size += 2;
+    outt.style.fontSize = size + "px";
+}
+
+function decfont(){
+    var outt = document.getElementById("out");
+    size -= 2;
+    outt.style.fontSize = size + "px";
+}
